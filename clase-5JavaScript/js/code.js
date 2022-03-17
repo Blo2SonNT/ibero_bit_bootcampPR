4039e951ccb779d353e86b5aec466b415f083a61
Miguel Nieto <migustanieto.2@gmail.com>
2022-03-17 12:38:36 -0500

    Bootstrap - JavaScript - Refuerzo CSS

---
+++ clase-5JavaScript/js/code.js
@@ -0,0 +1,51 @@
+// var nombre = "freddy"
+// let apellido = "nieto"
+// console.log("%chola mundo", "color: red; font-size: 3rem")
+// console.log(nombre)
+
+// console.log(`Hola ${nombre}, como estas?`)
+// console.log("Hola " + nombre + ", como estas?")
+
+
+/*
+cometario
+en
+bloque
+*/
+
+
+// comentario de linea
+
+// var nombre = "fulano"
+// apellido = "perez"
+// const cedula = "946165186"
+// console.log(nombre)
+
+// let estudiantes = ['juan', 'sara', 'freddy', 'yofrey', 59, true]
+// debugger
+// let nombreUser = prompt("cual es su nombre?")
+
+// let apellidoUser = prompt("cual es su apellido?")
+
+// let edadUser = prompt("cual es su edad?")
+
+// let correoUser = prompt("cual es su correo?")
+
+// function Saludar(nombre_user, apellidito) {
+//     return `hola ${nombre_user} ${apellidito}`
+// }
+
+// function datos(edad_User, correo_User) {
+//     return `informacion adicional ${edad_User} ${correo_User}`
+// }
+
+// console.log(Saludar(nombreUser, apellidoUser))
+
+// console.log(datos(edadUser, correoUser))
+
+
+// alert("hola, soy una alerta :D")
+
+let tituloHtml = document.getElementById("titulo")
+console.log(tituloHtml)
+tituloHtml.classList.add('text-center', 'tiulo-color')

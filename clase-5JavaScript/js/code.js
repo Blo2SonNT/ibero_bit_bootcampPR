c553335d0273960953dde15d008869ec71c377ff
Miguel Nieto <migustanieto.2@gmail.com>
2022-03-17 19:53:22 -0500

    Javascript

--- clase-5JavaScript/js/code.js
+++ clase-5JavaScript/js/code.js
@@ -46,6 +46,69 @@ bloque
 
 // alert("hola, soy una alerta :D")
 
-let tituloHtml = document.getElementById("titulo")
-console.log(tituloHtml)
-tituloHtml.classList.add('text-center', 'tiulo-color')
+
+// let tituloHtml = document.getElementById("titulo")
+// tituloHtml.classList.add('text-center', 'tiulo-color')
+
+// let tituloHtml = document.querySelectorAll(".mi-titulo")
+// console.log(tituloHtml)
+
+// // document.write('hola')
+// var contenidoHtml = document.querySelector("#contenido")
+
+// contenidoHtml.innerHTML = `
+//     <h1 >Hola mundo</h1>
+//     <p class="fw-bold">Hoy la clase es virtual</p>
+
+// `
+
+// var cartasContainer = document.querySelector('#contenido')
+// console.log(cartasContainer)
+
+// cartasContainer.innerHTML = `
+// <div class="col">
+//     <div class="card">
+//       <div class="card-body">
+//         <h5 class="card-title">Card title</h5>
+//         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+//       </div>
+//     </div>
+//   </div>
+// `
+
+
+
+function cambia_alerta() {
+    let alertaHtml = document.querySelector("#alerta")
+    alertaHtml.classList.remove('alert-danger')
+    alertaHtml.classList.add('alert-success')
+    alertaHtml.innerHTML = `tarea completada`
+}
+
+function validar_formulario() {
+    var formHtml = document.getElementById("formulario")
+    formHtml.innerHTML += `
+        <div class="alert alert-danger mt-2" role="alert" id="alerta">
+            Llene todos los campos del formulario
+        </div>
+    `
+}
+
+function lista() {
+    let numeroUsuario = prompt("Hasta que numero desea la lista?")
+    let divNumeros = document.querySelector("#listaNumeros")
+    for (let x = 1; x <= numeroUsuario; x++) {
+        divNumeros.innerHTML += `
+        <div class="alert alert-dark" role="alert">
+            ${x}
+        </div>
+        `
+    }
+}
+
+//ciclos y condicionales
+// var y = 5
+// while (y <= 100) {
+//     console.log(y)
+//     y = parseInt(y) + 5
+// }
